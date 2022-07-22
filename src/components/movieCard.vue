c89923c63d86c9c4dff9700dc9e3a090d7275438
Ego Lydia <94656569+EgoLydia@users.noreply.github.com>
2022-07-22 19:44:50 +0100

    Add movie categories to home component

--- src/components/movieCard.vue
+++ src/components/movieCard.vue
@@ -1,69 +1,86 @@
 <template>
-    <div id="movie-card">
-        <img :src="imgUrl+movie.backdrop_path" class="movie-img"/>
-        <div class="text">
-            <div class="title">
-                <!-- <h5 v-if="movie.media_type">{{ movie.title }} ({{movie.release_date}})</h5> -->
-                <h5 v-if="!movie.media_type || movie.media_type == 'movie'">{{ movie.title }} ({{ movie.release_date }})</h5>
-                <h5 v-else>{{ movie.name }} ({{ movie.first_air_date }})</h5>
-            </div>
-            <div class="details">
-                <h5>{{ movie.vote_average.toFixed(1) }} Rating</h5>
-                <h5>{{ movie.release_date || movie.first_air_date }}</h5>
-            </div>
-        </div>
+  <div id="movie-card">
+    <img :src="imgUrl + movie.backdrop_path" class="movie-img" />
+    <div class="text">
+      <div class="title">
+        <h5 v-if="!movie.media_type || movie.media_type == 'movie'">
+          {{ movie.title }} ({{ getYear(movie.release_date) }})
+        </h5>
+        <h5 v-else>{{ movie.name }} ({{ getYear(movie.first_air_date) }})</h5>
+      </div>
+      <div class="details">
+        <h5>{{ movie.vote_average.toFixed(1) }} Rating</h5>
+        <h5>{{ fullDate(movie.release_date || movie.first_air_date) }}</h5>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import { IMG_URL } from './../config.js'
+import { IMG_URL } from "./../config.js";
+import { getFullDate, getFullYear } from "../utils/dateUtils";
 
 export default {
-    props: {
-        movie: {
-            type: Object
-        }
+  props: {
+    movie: {
+      type: Object,
     },
-    data () {
-        return {
-            imgUrl: IMG_URL
-        }
-    }
-}
+  },
+  data() {
+    return {
+      imgUrl: IMG_URL,
+    };
+  },
+  methods: {
+    getYear(date) {
+      return getFullYear(date);
+    },
+    fullDate(date) {
+      return getFullDate(date);
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-    h5 {
-        margin: 0;
-        margin-top: 10px;
-    }
-    
-    .text {
-        padding: 10px;
-        padding-bottom: 50px;
-    }
+h5 {
+  margin: 0;
+  margin-top: 10px;
+  font-size: 13px;
+}
 
-    .title {
-        h5 {
-            height: 20px;
-        }
-    }
+.text {
+  padding: 10px;
+  padding-bottom: 50px;
+}
 
-    #movie-card {
-        border: 1px solid #ccc;
-        // max-width: 300px;
-        // box-shadow: 1px 2px 20px #ccc;
-    }
+.title {
+  h5 {
+    height: 20px;
+  }
+}
 
-    .movie-img {
-        height: 180px;
-        object-fit: cover;
-        width:100%;
-    }
+#movie-card {
+  border: 1px solid #ccc;
+  // max-width: 300px;
+  // box-shadow: 1px 2px 20px #ccc;
+}
 
-    .details {
-        display: flex;
-        justify-content: space-between;
-        margin-top: 20px;
-    }
+.movie-img {
+  height: 180px;
+  object-fit: cover;
+  width: 100%;
+}
+
+.details {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+}
+
+@media (min-width: 768px) {
+  h5 {
+    font-size: 13px;
+  }
+}
 </style>
--- src/components/movieList.vue
+++ src/components/movieList.vue
@@ -1,60 +1,71 @@
 <template>
-    <div>
-        <h3>{{heading}}</h3>
-        <div id="movie-list">
-            <div class="items" v-for="movie in movies" :key="movie.id">
-                <movie-card-vue :movie="movie" />
-            </div>
-        </div>
+  <div>
+    <h4 v-if="!isLoading">{{ heading }}</h4>
+    <div id="movie-list">
+      <div class="items" v-for="movie in movies" :key="movie.id">
+        <movie-card-vue :movie="movie" />
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import movieCardVue from './movieCard.vue'
-import Axios from 'axios'
-import { API_KEY, BASE_URL } from './../config.js'
+import movieCardVue from "./movieCard.vue";
+import Axios from "axios";
+import { API_KEY, BASE_URL } from "./../config.js";
 
 export default {
-    components: {
-        movieCardVue
+  components: {
+    movieCardVue,
+  },
+  props: {
+    keyword: {
+      type: String,
+      default: "trending",
     },
-    props: {
-        keyword: {
-            type: String,
-            default: 'trending'
-        },
-        heading: {
-            type: String,
-            default: 'Trending this week'
-        },
-        numberOfMovies: {
-            type: Number,
-            default: 3
-        },
+    heading: {
+      type: String,
+      default: "Trending this week",
     },
-    data () {
-        return {
-            movies: []
-        }
+    numberOfMovies: {
+      type: Number,
+      default: 3,
     },
-    mounted() {
-        Axios.get(`${BASE_URL}${this.keyword}?api_key=${API_KEY}`).then(res => {
-            this.movies = res.data.results.slice(0, this.numberOfMovies)
-            console.log(this.movies)
-        })
-    }
-}
+  },
+  data() {
+    return {
+      movies: [],
+      isLoading: false,
+    };
+  },
+  mounted() {
+    this.isLoading = true;
+    Axios.get(`${BASE_URL}${this.keyword}?api_key=${API_KEY}`)
+      .then((res) => {
+        this.movies = res.data.results.slice(0, this.numberOfMovies);
+        this.isLoading = false;
+      })
+      .catch(() => (this.isLoading = false));
+  },
+};
 </script>
 
 <style scoped>
-    #movie-list {
-        display: flex;
-        flex-wrap: wrap;
-        gap: 1em;
-        justify-content: space-between;
-    }
+#movie-list {
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 8px;
+}
 
-    .items {
-        flex: 0 1 250px;
-    }
+@media (min-width: 450px) {
+  #movie-list {
+    grid-template-columns: 1fr 1fr;
+  }
+}
+
+@media (min-width: 768px) {
+  #movie-list {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+}
 </style>
